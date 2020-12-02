2ad7b66da917ea69a91fad04531f6258aa093ed7
dangvandat <dangdat.09071997@gmail.com>
2020-12-02 16:36:06 +0700

    datdv - commit

--- src/main/java/com/server/tradedoc/config/JpaTransactionManagerConfig.java
+++ src/main/java/com/server/tradedoc/config/JpaTransactionManagerConfig.java
@@ -78,7 +78,7 @@ public class JpaTransactionManagerConfig {
     @Bean
     public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setDriverClassName("org.postgresql.Driver");
         dataSource.setUrl(url);
         dataSource.setUsername(username);
         dataSource.setPassword(password);
